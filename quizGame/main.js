782b64c00ae276e30299cd09627fb1a95357efbf
James <thepago@gmail.com>
2019-08-02 16:52:29 -0400

    Added showResults function

--- quizGame/main.js
+++ quizGame/main.js
@@ -1,9 +1,10 @@
+(function(){
 //Variables
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-//Questions
+//Questions array
 const myQuestions = [
   {
     question: "What planet is closest to the Sun?",
@@ -37,34 +38,78 @@ const myQuestions = [
   }
 ];
 
+//Init function
 function buildQuiz(){
+    //Stores output
     const output = [];
     
     myQuestions.forEach(
-    currentQuestion, questionNumber) {
+    (currentQuestion, questionNumber) => {
+        
+        //Answer choices
         const answers = [];
         
-        for(letter in currentQuestion.answers) {
+        //For each available answer...
+        for(letter in currentQuestion.answers){
+            
+            //Adds HTML radio button to each answer
             answers.push(
-            <label>
-                <input type="radio" name="question${questionNumber}" value="${letter}"
-            ${letterr} : ${currentQuestion.answers[letter]}
-            </label>
+            `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} : ${currentQuestion.answers[letter]}
+            </label>`
             );
         }
-    }
-    
+    //Adds question and its answers to output
     output.push(
-    <div class="question"> ${currentQuestion.question}</div>
-    <div class="answers"> ${answers.join('')}</div>
+    `<div class="question"> ${currentQuestion.question}</div>
+    <div class="answers"> ${answers.join('')}</div>`
             );
-    quizContainer.innerHTML = output.join('');
-}
+        }
+    );
+    
+        //Combines output lists
+        quizContainer.innerHTML = output.join('');
+    }
 
-function showResults(){}
+function showResults(){
+    
+    //Gather answers from quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    
+    //Keep track of answers
+    let numCorrect = 0;
+    
+    //For each question...
+    myQuestions.forEach((currrentQuestion, questionNumber) => {
+        
+        //Find answer that is selected
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        
+        //If else
+        if(userAnswer===currentQuestion.correctAnswer){
+            
+            //Add to number of correct
+            numCorrect++;
+            
+            //Colors the answer green
+            answerContainers[questionNumber].style.color = 'green';
+        } else{
+            
+            //If answer is wrong, colors the answers red
+            answerContainers[questionNumber].style.color = 'red';
+        }
+    });
+    
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
 
 // display quiz right away
 buildQuiz();
 
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+    })();
